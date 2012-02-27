85e79d09967176b7b41e001b9bf2b3dd06a5cfcd
oxydia <contact@oxydia.com>
2012-02-27 16:30:13 +0100

    Re-init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchizoWorld::Application.config.secret_token = '96abc57fbbc11333626a22e2b59a6c4eca7e195cb77d369c2c9c0374d9fef35e764955ac8c72fbb3f8d2ea81c2803d0194e5a1e601558b3d6cf2f13a7ce78921'
